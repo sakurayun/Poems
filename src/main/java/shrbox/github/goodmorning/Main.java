2e7da4111ec5c334b316dcceb78b421b971b9332
ShrBox <shrbox@outlook.com>
2020-08-05 12:56:31 +0800

    configuration file

--- src/main/java/shrbox/github/goodmorning/Main.java
+++ src/main/java/shrbox/github/goodmorning/Main.java
@@ -1,12 +1,29 @@
 package shrbox.github.goodmorning;
 
+import net.mamoe.mirai.console.plugins.Config;
 import net.mamoe.mirai.console.plugins.PluginBase;
 import net.mamoe.mirai.message.GroupMessageEvent;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 class Main extends PluginBase {
+    public Config config;
+    public void loadCfg() {
+        config = loadConfig("config.yml");
+        List<Long> temp_list = new ArrayList<>();
+        Collections.addAll(temp_list, 1145141919L, 1919810L);
+        config.setIfAbsent("enableGroups", temp_list);
+        config.save();
+        temp_list.clear();
+    }
+
     public void onEnable() {
+        loadCfg();
         getEventListener().subscribeAlways(GroupMessageEvent.class, (GroupMessageEvent e) -> {
             if (e.getMessage().contentToString().equals("早")) {
+                if (!config.getLongList("enableGroups").contains(e.getGroup().getId())) return;
                 EventThread eventThread = new EventThread();
                 eventThread.startThread(e);
             }
